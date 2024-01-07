51f99aa80aa6b18651c83cb6483cc64bc10e8179
Mehmet Ali MEŞE <147969652+mehalimes@users.noreply.github.com>
2024-01-07 17:37:54 +0300

    Delete todo-app-back-api/todo-app/Others/todo-app/todo-app-front-react directory

--- todo-app-back-api/todo-app/Others/todo-app/todo-app-front-react/src/app/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import appReducer from "../slices/appSlice.jsx";
-
-export default configureStore({
-    reducer: {
-        app: appReducer,
-    },
-})
